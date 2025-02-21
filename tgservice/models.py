5db83059361cfaf28d3bccb7c47937aa09448ddc
Raa5ty <rusman482@gmail.com>
2025-02-22 00:54:06 +0400

    Добавлено папка для системных команд в tgservice

--- tgservice/models.py
+++ tgservice/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
 # Create your models here.
+# class Category(models.Model):
+#     # Id не надо, он уже сам появиться
+#     name = models.CharField(max_length=16, unique=True)
+#     description = models.TextField(blank=True)
+
+#     # Основные типы полей
+#     # дата
+#     # models.DateField
+#     # models.DateTimeField
+#     # models.TimeField
+#     # # Числа
+#     # models.IntegerField
+#     # models.PositiveIntegerField
+#     # models.PositiveSmallIntegerField
+#     # models.FloatField
+#     # models.DecimalField
+#     # # Логический
+#     # models.BooleanField
+#     # # Байты (blob)
+#     # models.BinaryField
+#     # # Картинка
+#     # models.ImageField
+#     # # Файл
+#     # models.FileField
+#     # # url, email
+#     # models.URLField
+#     # models.EmailField
+
+#     def __str__(self):
+#         return self.name
